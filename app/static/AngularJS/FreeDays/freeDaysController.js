997cb5125a40fb13187c3c2b6cbaa59e89ec3dfd
hhipolito <hhipolito@bism.com.mx>
2016-05-25 10:08:14 -0500

    inprogress
    modification in progress

--- app/static/AngularJS/FreeDays/freeDaysController.js
+++ app/static/AngularJS/FreeDays/freeDaysController.js
@@ -8,6 +8,38 @@ registrationModule.controller('freeDaysController', function($scope, alertFactor
         freeDaysRepository.getFreeDays().then(function(result) {
             if (result.data.length > 0) {
                 $scope.allDays = result.data;
+                setTimeout(function() {
+                    $('.dataTables-example').DataTable({
+                        dom: '<"html5buttons"B>lTfgitp',
+                        buttons: [{
+                                extend: 'copy'
+                            }, {
+                                extend: 'csv'
+                            }, {
+                                extend: 'excel',
+                                title: 'ExampleFile'
+                            }, {
+                                extend: 'pdf',
+                                title: 'ExampleFile'
+                            },
+
+                            {
+                                extend: 'print',
+                                customize: function(win) {
+                                    $(win.document.body).addClass('white-bg');
+                                    $(win.document.body).css('font-size', '10px');
+
+                                    $(win.document.body).find('table')
+                                        .addClass('compact')
+                                        .css('font-size', 'inherit');
+                                }
+                            }
+                        ]
+
+                    });
+
+                }, 1000);
+
                 alertFactory.success("Datos cargados");
             } else {
                 alertFactory.info("No se encontraron datos");
